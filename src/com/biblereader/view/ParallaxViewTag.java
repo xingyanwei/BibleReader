28d19211522197ce2a00bd93691a88cd552302dc
xingyanwei <xing.yw@DESKTOP-S0EEFHD>
2015-08-21 21:38:39 +0800

    修改欢迎界面

---
+++ src/com/biblereader/view/ParallaxViewTag.java
@@ -0,0 +1,11 @@
+package com.biblereader.view;
+
+public class ParallaxViewTag {
+  protected int index;
+  protected float xIn;
+  protected float xOut;
+  protected float yIn;
+  protected float yOut;
+  protected float alphaIn;
+  protected float alphaOut;
+}
